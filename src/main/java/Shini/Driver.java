d713482d3ccf3003bbaa0131edea3ba1c5672ae9
Ali <ali1955.2011@gmail.com>
2024-12-23 16:47:52 +0200

    Create Fonts Method to make it more readability

--- src/main/java/Shini/Driver.java
+++ src/main/java/Shini/Driver.java
@@ -8,6 +8,7 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Driver extends Application {
     public static Stage loginStage;
@@ -22,16 +23,20 @@ public class Driver extends Application {
         loginStage = stage;
         /* Compile JavaFX Application class with Java 20 **/
         /* Load Fonts, Cairo initialized to Intellij **/
-        Font.loadFont(Driver.class.getResource("/Shini/Fonts/Cairo-Regular.ttf").toExternalForm(), 10);
-        Font.loadFont(Driver.class.getResource("/Shini/Fonts/Cairo-Bold.ttf").toExternalForm(), 10);
-        Font.loadFont(Driver.class.getResource("/Shini/Fonts/Cairo-SemiBold.ttf").toExternalForm(), 10);
-        Font.loadFont(Driver.class.getResource("/Shini/Fonts/Cairo-Black.ttf").toExternalForm(), 10);
+        initializeFonts();
         FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("/Shini/FXML/LoginUI.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 606, 600);
         loginStage.setTitle("Shini Extra Online");
         loginStage.setScene(scene);
         loginStage.show();
+    }
+    
+    private void initializeFonts() {
+        Font.loadFont(Objects.requireNonNull(Driver.class.getResource("/Shini/Fonts/Cairo-Regular.ttf")).toExternalForm(), 10);
+        Font.loadFont(Objects.requireNonNull(Driver.class.getResource("/Shini/Fonts/Cairo-Bold.ttf")).toExternalForm(), 10);
+        Font.loadFont(Objects.requireNonNull(Driver.class.getResource("/Shini/Fonts/Cairo-SemiBold.ttf")).toExternalForm(), 10);
+        Font.loadFont(Objects.requireNonNull(Driver.class.getResource("/Shini/Fonts/Cairo-Black.ttf")).toExternalForm(), 10);
     }
 
     public static void main(String[] args) {
