7071d4c08d36b4a47c52bb83c7c5303cb8c965f1
Hanade Zareer <hanadezareer6@gmail.com>
2024-12-26 17:43:41 +0200

    create database connection with its helper class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module shiniproject.shinigo {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.compiler;
+    requires java.sql;
 
     opens Shini to javafx.fxml;
     exports Shini;
